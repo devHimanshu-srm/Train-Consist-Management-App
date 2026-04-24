82cda1ee61720d15772f58a6dc578750a96c7828
devHimanshu-srm <devhimanshu311@gmail.com>
2026-04-24 15:23:41 +0530

    UC16 - Bubble sort for passenger bogie capacities

---
+++ src/UC16.java
@@ -0,0 +1,40 @@
+public class UC16 {
+
+    public static void main(String[] args) {
+
+        // Step 1: Create array of capacities
+        int[] capacities = {50, 20, 80, 10, 60};
+
+        System.out.println("Before Sorting:");
+        printArray(capacities);
+
+        // Step 2: Bubble Sort
+        int n = capacities.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                // Compare adjacent elements
+                if (capacities[j] > capacities[j + 1]) {
+
+                    // Swap
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nAfter Sorting:");
+        printArray(capacities);
+    }
+
+    // Helper method to print array
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
